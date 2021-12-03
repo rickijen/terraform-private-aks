3b7525178e91fe72902983a4c2136996670b7771
Rick Jen <rickijen@gmail.com>
2021-12-02 21:42:47 -0800

    added log analytics workspace name as output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "ssh_command" {
   value = "ssh ${module.jumpbox.jumpbox_username}@${module.jumpbox.jumpbox_ip}"
 }
 
+output "azurerm_log_analytics_workspace_name" {
+  value = azurerm_log_analytics_workspace.default.name
+}
+
 #output "jumpbox_password" {
 #  description = "Jumpbox Admin Passowrd"
 #  value       = module.jumpbox.jumpbox_password
