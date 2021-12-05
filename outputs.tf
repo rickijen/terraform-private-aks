5e037f6da0c88676aa8c98f3b08a34e53b974a22
Rick Jen <rickijen@gmail.com>
2021-12-04 22:05:48 -0800

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "azurerm_log_analytics_workspace_name" {
   value = azurerm_log_analytics_workspace.default.name
 }
 
+output "kube_vnet_name" {
+  value = "${random_pet.prefix.id}-kube-vnet"
+}
+
+output "aks_subnet_prefix" {
+  value = "10.10.5.0/24"
+}
+
 #output "jumpbox_password" {
 #  description = "Jumpbox Admin Passowrd"
 #  value       = module.jumpbox.jumpbox_password
